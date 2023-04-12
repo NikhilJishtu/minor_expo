3e5b3de03e0ae663be2bff7d8b375854d1859f5c
Nikhil Jishtu <95859662+NikhilJishtu@users.noreply.github.com>
2023-04-12 09:54:37 +0200

    commit

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ class Soldier {
         this.pos = pos;
         this.color = color;
         this.r = r;
+        this.maxHealth = health;
         this.health = health;
         this.attack = attack;
 
@@ -31,42 +32,81 @@ class Soldier {
         this.minTargetDist = 2;
     }
 
-update() {
-    if (this.currentTarget == null) return;
+    update() {
+        if (this.currentTarget == null) return;
 
-    let dir = new Vector(this.currentTarget.x - this.pos.x, this.currentTarget.y - this.pos.y);
-    let distance = (dir.x ** 2 + dir.y ** 2) ** (1 / 2);
+        let dir = new Vector(this.currentTarget.x - this.pos.x, this.currentTarget.y - this.pos.y);
+        let distance = (dir.x ** 2 + dir.y ** 2) ** (1 / 2);
 
-    if (distance == 0) return;
+        if (distance == 0) return;
 
-    dir.x /= distance;
-    dir.y /= distance;
+        dir.x /= distance;
+        dir.y /= distance;
 
-    this.pos.x += dir.x * this.speed;
-    this.pos.y += dir.y * this.speed;
+        this.pos.x += dir.x * this.speed;
+        this.pos.y += dir.y * this.speed;
 
-    let xDist = Math.abs(this.pos.x - this.currentTarget.x);
-    let yDist = Math.abs(this.pos.y - this.currentTarget.y);
+        let xDist = Math.abs(this.pos.x - this.currentTarget.x);
+        let yDist = Math.abs(this.pos.y - this.currentTarget.y);
 
-    if (xDist <= this.minTargetDist && yDist <= this.minTargetDist) {
-        this.targets.splice(0, 1);
+        if (xDist <= this.minTargetDist && yDist <= this.minTargetDist) {
+            this.targets.splice(0, 1);
 
-        if (this.targets.length == 0) {
-            this.currentTarget = null;
-        }
+            if (this.targets.length == 0) {
+                this.health -= 10;
+                if (this.health <= 0) {
+                    let index = soldiers.indexOf(this);
+                    if (index != -1) {
+                        soldiers.splice(index, 1);
+                    }
+                }
 
-        else {
-            this.currentTarget = this.targets[0];
+                else {
+
+                    this.targets = [];
+                    this.targets[0] = new Vector(startPos.x + pathData[0].x, startPos.y + pathData[0].y);
+                    for (let i = 1; i < pathData.length; i++) {
+                        let prevTarget = this.targets[i - 1];
+                        let path = pathData[i];
+                        let newTarget = new Vector(prevTarget.x + path.x, prevTarget.y + path.y);
+                        this.targets[i] = newTarget;
+                    }
+                    this.currentTarget = this.targets[0];
+                }
+            }
+            else {
+                this.currentTarget = this.targets[0];
+            }
         }
     }
-}
 
-render(){
-    context.fillStyle = this.color;
-    context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI * 2);
-    context.fill();
-}
+    renderHealthBar() {
+        let x = this.pos.x - this.r;
+        let y = this.pos.y + this.r + 5;
+        let w = this.r * 2;
+        let h = 5;
+        let healthPct = this.health / this.maxHealth;
+        let healthColor = "green";
+        if (healthPct < 0.5) {
+            healthColor = "yellow";
+        }
+        if (healthPct < 0.2) {
+            healthColor = "red";
+        }
+        context.fillStyle = "gray";
+        context.fillRect(x, y, w, h);
+        context.fillStyle = healthColor;
+        context.fillRect(x, y, w * healthPct, h);
+    }
+
+    render() {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI * 2);
+        context.fill();
+        this.renderHealthBar();
+    }
+
 }
 
 
@@ -94,16 +134,16 @@ let pathData = [
 let soldiers = [];
 const NUM_SOLDIERS = 10;
 
-let soldierStart = new Vector(100,0);
+let soldierStart = new Vector(100, 0);
 
 for (let i = 0; i < NUM_SOLDIERS; i++) {
-    let newSoldier = new Soldier(new Vector(soldierStart.x,soldierStart.y),"blue",20,100,10);
+    let newSoldier = new Soldier(new Vector(soldierStart.x, soldierStart.y), "blue", 20, 100, 10);
     soldiers[soldiers.length] = newSoldier;
     soldierStart.y -= 100;
 }
 
 function update() {
-    soldiers.forEach(function(s) {
+    soldiers.forEach(function (s) {
         s.update();
     });
 }
@@ -167,7 +207,7 @@ function render() {
 
     renderPath();
     renderGrid();
-    soldiers.forEach(function(s) {
+    soldiers.forEach(function (s) {
         s.render();
     });
 }

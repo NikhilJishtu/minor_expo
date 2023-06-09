2aa2ea4b5741b5c5e0f3d91507e8913ca6f434cd
GekkePerp <95859662+NikhilJishtu@users.noreply.github.com>
2023-06-09 13:15:47 +0200

    commit

--- towerdefense/script.js
+++ towerdefense/script.js
@@ -244,7 +244,7 @@ var sniper = {
   dmg: 10, // 10
   fireRate: 100, // 20
   fire: 0, // always 0
-  range: 150, // 100
+  range: 300, // 100
   color: "blue",
   strokeColor: "black",
   level: 0,
@@ -252,31 +252,31 @@ var sniper = {
     lvl1:{
       dmg: 10,
       fireRate: 100,
-      range: 150,
+      range: 320,
       price: 40,
     },
     lvl2:{
       dmg: 15,
       fireRate: 90,
-      range: 180,
+      range: 350,
       price: 30,
     },
     lvl3:{
       dmg: 15,
       fireRate: 85,
-      range: 210,
+      range: 380,
       price: 30,
     },
     lvl4:{
       dmg: 18,
       fireRate: 80,
-      range: 240,
+      range: 410,
       price: 30,
     },
     lvl5:{
       dmg: 25,
       fireRate: 70,
-      range: 300,
+      range: 500,
       price: 50,
     },
   }
